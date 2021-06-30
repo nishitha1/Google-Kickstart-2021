d4fc72404e3297480e717940db02cd9d58558b8b
Nishitha <32068334+nishitha1@users.noreply.github.com>
2021-06-29 19:44:22 -0700

    Update k-goodness string.py

--- Round A/k-goodness string.py
+++ Round A/k-goodness string.py
@@ -1,3 +1,6 @@
+# Google Kick Start 2021 Round A - K-Goodness String
+# https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068cca3
+
 # if s[i] != s[n-i+1]
 # goodness score += 1
 class Solution:
