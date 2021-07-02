147af0a97c9b7d86f1a60ebbce07efc63e5c71df
Nishitha <32068334+nishitha1@users.noreply.github.com>
2021-07-01 23:24:11 -0700

    Update Checksum.py

--- Round A/Checksum.py
+++ Round A/Checksum.py
@@ -1,6 +1,6 @@
-# finding a minimum weight subset of edges that breaks all cycles.
-#the problem of finding a minimum weight cycle breaking 
-#edge set is equivalent to the well known problem of 
-#finding a maximum weight spanning forest of G, 
-#except that we would build the complement set 
-#of edges to keep rather than the set of edges to remove.
+# Google Kick Start 2021 Round A - Checksum
+# https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068c2c3
+
+# Find a minimum weight subset of edges that breaks all cycles.
+# The problem of finding a minimum weight cycle breaking edge set is equivalent to the well known problem of 
+# finding a maximum weight spanning forest of G, except that we would build the complement set of edges to keep rather than the set of edges to remove.
