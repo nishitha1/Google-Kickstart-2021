1a809e99ac66e93a40103416b09de07e28dc7432
Nishitha <32068334+nishitha1@users.noreply.github.com>
2021-07-02 22:32:24 -0700

    Update Increasing Substring.py

--- Round B/Increasing Substring.py
+++ Round B/Increasing Substring.py
@@ -2,6 +2,20 @@
 # https://codingcompetitions.withgoogle.com/kickstart/round/0000000000435a5b/000000000077a882
 # Time: O(N)
 
+##def longestStrictlyIncreasingSubstring(n, s):
+##    prev_l = 0
+##    for i in range(n):
+##        if i == 0:
+##            prev_l = 1
+##        elif i > 0:
+##            if s[i-1] < s[i]:
+##                prev_l = prev_l + 1
+##            else:
+##                prev_l
+##        print(prev_l, end = ' ')
+##    print()
+
+
 def longestStrictlyIncreasingSubstring():
     n = int(input())
     start = 'A'
