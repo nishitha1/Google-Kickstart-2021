897cf1fb079464a7f9ed781cc5ac44be5857e96c
Nishitha <32068334+nishitha1@users.noreply.github.com>
2021-07-07 13:38:26 -0700

    Update smaller_strings.py

--- Round C/smaller_strings.py
+++ Round C/smaller_strings.py
@@ -1,3 +1,8 @@
+# Google Kick Start 2021 Round C - Smaller Strings
+# https://codingcompetitions.withgoogle.com/kickstart/round/0000000000435c44/00000000007ebe5e
+
+# Referred: https://github.com/kamyu104/GoogleKickStart-2021/blob/main/Round%20C/smaller_strings.py
+
 # input: String s
 # output: Number of palindrome strings of length N 
 # that is lexicographically smaller than s
@@ -5,6 +10,7 @@
 # (not necessarily palindromes) that are lexicographically 
 # smaller than S′
 # bc - "aa", "bb" 
+
 MOD = 10 ** 9 + 7
 def compute(s, n, k):
     count = 0
